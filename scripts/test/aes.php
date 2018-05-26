0b4a639f15781891085bb228e3d17911ff54e7bb
Qrux <qrux@xlapp.org>
2018-05-26 14:49:58 +0100

    Adding crypto & QR code APIs.

---
+++ scripts/test/aes.php
@@ -0,0 +1,310 @@
+#! /usr/bin/php
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * Date: 4/25/14
+ * Time: 8:12 AM
+ *
+ * Copyright (c) 2012 Troy Wu
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+require_once(__DIR__ . "/../../inc/utils.php");
+
+function main ( $argc, $argv )
+{
+    // Test using NIST test vectors.
+
+    $plainHex1 = "6bc1bee22e409f96e93d7e117393172a";
+    $plainHex2 = "ae2d8a571e03ac9c9eb76fac45af8e51";
+    $plainHex3 = "30c81c46a35ce411e5fbc1191a0a52ef";
+    $plainHex4 = "f69f2445df4f9b17ad2b417be66c3710";
+    $plainHex  = $plainHex1 . $plainHex2 . $plainHex3 . $plainHex4;
+    $plain     = hex2str($plainHex);
+
+    $cipherHex1         = "601ec313775789a5b7a7f504bbf3d228";
+    $cipherHex2         = "f443e3ca4d62b59aca84e990cacaf5c5";
+    $cipherHex3         = "2b0930daa23de94ce87017ba2d84988d";
+    $cipherHex4         = "dfc9c58db67aada613c2dd08457941a6";
+    $cipherHex          = $cipherHex1 . $cipherHex2 . $cipherHex3 . $cipherHex4;
+    $expectedCiphertext = hex2str($cipherHex);
+
+    $keyHex = "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4";
+    $key    = hex2str($keyHex);
+
+    $ctrHex = "f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
+    $ctr    = hex2str($ctrHex);
+
+    clog("       key (hex)", $keyHex);
+    clog("       ctr (hex)", $ctrHex);
+    clog(" plaintext (hex)", $plainHex);
+
+    // test_mcrypt(...); // No longer using this, since Rijndal != AES.
+
+    test1($key, $ctr, $plain, $expectedCiphertext);
+    test2($key, $ctr, $plain, $expectedCiphertext);
+    test3($key, $plain);
+    test4($key, $plain);
+}
+
+/**
+ * Uses phpseclib directly.
+ *
+ * @param $key
+ * @param $iv
+ * @param $plaintext
+ * @param $expectedCiphertext
+ *
+ * @return bool - (true) if (plain === recovered) && (cipher === expected); (false) otherwise.
+ */
+function test1 ( $key, $iv, $plaintext, $expectedCiphertext )
+{
+    $aes = new Crypt_AES(CRYPT_AES_MODE_CTR);
+    //$cipher->setPassword('whatever');
+    $aes->setKey($key);
+    $aes->setIV($iv);
+
+    // the following does the same thing:
+    //$cipher->setPassword('whatever', 'pbkdf2', 'sha1', 'phpseclib/salt', 1000, 256 / 8);
+    //$cipher->setIV('...'); // defaults to all-NULLs if not explicitely defined
+
+    //$size      = 10 * 1024;
+    //$plaintext = str_repeat('a', $size);
+
+    $ciphertext = $aes->encrypt($plaintext);
+    $recovered  = $aes->decrypt($ciphertext);
+
+    return verify("test1", $plaintext, $expectedCiphertext, $ciphertext, $recovered);
+}
+
+/**
+ * Uses FJ-refactored AES calls.
+ *
+ * @param $key
+ * @param $iv
+ * @param $plaintext
+ * @param $expectedCiphertext
+ *
+ * @return bool - (true) if (plain === recovered) && (cipher === expected); (false) otherwise.
+ */
+function test2 ( $key, $iv, $plaintext, $expectedCiphertext )
+{
+    $ciphertext = FJ::encrypt($key, $iv, $plaintext);
+    $recovered  = FJ::decrypt($key, $iv, $ciphertext);
+
+    return verify("test2", $plaintext, $expectedCiphertext, $ciphertext, $recovered);
+}
+
+/**
+ * Uses FJ-refactored AES calls, testing if IV matters.
+ *
+ * Tests MISMATCHED-CTR-IV (last byte different) -- OBV. DOES NOT WORK!
+ *
+ * @param $key
+ * @param $plaintext
+ *
+ * @return bool - (true) if (plain === recovered) && (cipher === expected); (false) otherwise.
+ */
+function test3 ( $key, $plaintext )
+{
+    $ctrHex1 = "f0f1f2f3f4f5f6f7f8f9fafbfcfdfe00";
+    $ctrHex2 = "f0f1f2f3f4f5f6f7f8f9fafbfcfdfe01";
+
+    $ctr1 = hex2str($ctrHex1);
+    $ctr2 = hex2str($ctrHex2);
+
+    $ciphertext = FJ::encrypt($key, $ctr1, $plaintext);
+    $recovered  = FJ::decrypt($key, $ctr2, $ciphertext);
+
+    return verify("test3", $plaintext, false, $ciphertext, $recovered, false);
+}
+
+/**
+ * Uses FJ-refactored AES calls.
+ *
+ * Tests MATCHING-BUT-BAD-CTR-IV (wrong bit-length) -- WORKS!
+ *
+ * @param $key
+ * @param $plaintext
+ *
+ * @return bool - (true) if (plain === recovered) && (cipher === expected); (false) otherwise.
+ */
+function test4 ( $key, $plaintext )
+{
+    $ciphertext = FJ::encrypt($key, "hello", $plaintext);
+    $recovered  = FJ::decrypt($key, "hello", $ciphertext);
+
+    return verify("test4", $plaintext, false, $ciphertext, $recovered);
+}
+
+/**
+ * Checks results, prints output.
+ *
+ * @param $plaintext
+ * @param $expectedCiphertext
+ * @param $ciphertext
+ * @param $recovered
+ * @param $isExpectedPass - (true) if expected-to-pass; (false) otherwise.
+ *
+ * @return bool - (true) if (plain === recovered) && (cipher === expected); (false) otherwise.
+ */
+function verify ( $mesg, $plaintext, $expectedCiphertext, $ciphertext, $recovered, $isExpectedPass = true )
+{
+
+    $plainHex = str2hex($plaintext);
+    $outHex   = str2hex($ciphertext);
+
+    // NOTE - Not always comparing to known encrypted result.
+    if ( false !== $expectedCiphertext )
+    {
+        $cipherHex = str2hex($expectedCiphertext);
+
+        clog("$mesg -             expected ciphertext (hex)", $cipherHex);
+        clog("$mesg - Pure-PHP AES (phpseclib) output (hex)", $outHex);
+
+        clog("$mesg -   expected ciphertext length", strlen($expectedCiphertext));
+        clog("$mesg -     actual ciphertext length", strlen($ciphertext));
+
+        $isEncryptionWorking = ($ciphertext === $expectedCiphertext);
+        //$isEncryptionWorking = false;
+
+        clog("$mesg - Pure-PHP AES (phpseclib) encryption worked?", $isEncryptionWorking);
+
+        if ( $isExpectedPass )
+        {
+            if ( !$isEncryptionWorking )
+            {
+                FJ::error("$mesg - Pure-PHP AES (phpseclib) ENCRYPTION not working.");
+            }
+        }
+        else
+        {
+            if ( $isEncryptionWorking )
+            {
+                FJ::error("$mesg - Pure-PHP AES (phpseclib) ENCRYPTION not working.");
+            }
+        }
+    }
+
+    $recHex              = str2hex($recovered);
+    $isDecryptionWorking = ($recovered === $plaintext);
+    //$isDecryptionWorking = false;
+
+    clog("$mesg -                  expected recovered-plaintext (hex)", $recHex);
+    clog("$mesg - Pure-PHP AES (phpseclib) recovered-plainetext (hex)", $plainHex);
+
+    determineStatus($mesg, "Pure-PHP AES (phpseclib) decryption worked?", $isDecryptionWorking, $isExpectedPass);
+
+
+    clog("$mesg - Pure-PHP AES (phpseclib) decryption worked?", $isDecryptionWorking);
+
+    if ( $isExpectedPass )
+    {
+        if ( !$isDecryptionWorking )
+        {
+            FJ::error("$mesg - Pure-PHP AES (phpseclib) DECRYPTION not working.");
+        }
+    }
+    else
+    {
+        if ( $isDecryptionWorking )
+        {
+            FJ::error("$mesg - Pure-PHP AES (phpseclib) DECRYPTION not working.");
+        }
+	else
+	{
+            clog("$mesg - Expected failure");
+	}
+    }
+
+    if ( false === $expectedCiphertext )
+    {
+        return $isDecryptionWorking;
+    }
+    else
+    {
+        return $isEncryptionWorking && $isDecryptionWorking;
+    }
+}
+
+function determineStatus ( $mesg, $base, $actual, $expected )
+{
+    $isGood = ( $expected === $actual );
+
+    if ( $isGood && $actual )
+    {
+        clog("$mesg - $base", "PASS");
+    }
+    else
+    {
+        clog("$mesg - $base", "EXPECTED-FAIL");
+    }
+}
+
+
+/**
+ * Uses mcrypt, which does not support AES.
+ *
+ * @return bool - (true) if (plain === recovered) && (cipher === expected); (false) otherwise.
+ */
+function test_mcrypt ()
+{
+    //clog("ciphertext (hex)", $cipherHex);
+
+    /*
+    $ivSize128  = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, 'ctr');
+    $ivSize192  = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_192, 'ctr');
+    $ivSize256  = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, 'ctr');
+    $isCTRBlock = mcrypt_module_is_block_algorithm_mode('ctr');
+
+    clog("AES-128 - IV (CTR) length", $ivSize128);
+    clog("AES-192 - IV (CTR) length", $ivSize192);
+    clog("AES-256 - IV (CTR) length", $ivSize256);
+    clog("CTR (per NIST)     length", strlen($ctr));
+    clog("CTR is block mode in PHP?", $isCTRBlock);
+
+    $td = mcrypt_module_open('rijndael-256', '', 'ecb', '');
+    mcrypt_generic_init($td, $key, $ctr);
+    $out    = mcrypt_generic($td, $plain);
+    $outHex = str2hex($out);
+
+    clog("       key (hex)", $keyHex);
+    clog("       ctr (hex)", $ctrHex);
+    clog(" plaintext (hex)", $plainHex);
+    clog("ciphertext (hex)", $cipherHex);
+    clog("    output (hex)", $outHex);
+
+    if ( $cipher == $out )
+    {
+        clog("mcrypt (Rijndal-256 used as AES-256) encryption worked?", true);
+    }
+    else
+    {
+        clog("mcrypt (Rijndal-256 used as AES-256) encryption worked?", false);
+    }
+    */
+
+    return false;
+}
